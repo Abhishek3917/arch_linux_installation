8625565d6d7872dcd574aa21fa4b132e07a7f016
abhishek <abhishekskumar709@gmail.com>
2024-03-21 00:27:04 +0530

    fixed bug

--- arch_install.sh
+++ arch_install.sh
@@ -24,12 +24,14 @@ if cat /sys/firmware/efi/fw_platform_size >/dev/null 2>&1; then # checking for u
     read HOME
     echo "do u need swap partition: (y/n) "
     read swap_need
-    if [[ $swap_need == 'y' ]]; then
-        echo "Please enter SWAP paritition: (example /dev/sda4)"
-        read SWAP
-        mkswap $SWAP
-        swapon $SWAP
-    fi
+
+    # if [[ $swap_need == 'y' ]]; then
+    #     echo "Please enter SWAP paritition: (example /dev/sda4)"
+    #     read SWAP
+    #     mkswap $SWAP
+    #     swapon $SWAP
+    # fi
+
     # formating the partion and creating home and efi dir and mounting the partition(root,home,efi)
     echo -e "\nCreating Filesystems...\n"
     echo "do u need FORMAT HOME partition: (y/n) "
@@ -51,68 +53,67 @@ if cat /sys/firmware/efi/fw_platform_size >/dev/null 2>&1; then # checking for u
     echo "----------------------------------------------------------------------------------------------------------"
     echo "---Install essential packages---"
     echo "----------------------------------------------------------------------------------------------------------"
-    pacstrap -K /mnt base linux linux-firmware nano base-devel --noconfirm --needed
-    echo "----------------------------------------------------------------------------------------------------------"
-    echo "-- Setup Dependencies--"
-    echo "----------------------------------------------------------------------------------------------------------"
-    pacstrap /mnt networkmanager network-manager-applet wireless_tools nano git reflector --noconfirm --needed
+    pacstrap -K /mnt base linux linux-firmware nano --noconfirm --needed
+
+    # generating the genfstab
     echo "Generating an fstab file........."
     genfstab -U /mnt >> /mnt/etc/fstab
 
-# cat << 'REALEND' > /mnt/next.sh
+cat << 'REALEND' > /mnt/next.sh
 
-# # generating the genfstab
+# setting timezone
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+#localization
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+#create locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "----------------------------------------------------------------------------------------------------------"
+echo "---Setting up system---"
+echo "----------------------------------------------------------------------------------------------------------"
+echo "Enter the hostname"
+read HOSTNAME
+echo $HOSTNAME >> /etc/hostname
+cat <<EOF > /etc/hosts
+127.0.0.1	localhost
+::1			localhost
+127.0.1.1	$HOSTNAME.localdomain	$HOSTNAME
+EOF
+echo "root user password"
+passwd
+echo "enter the usernmae"
+read USER
+useradd -m $USER
+usermod -aG wheel,storage,power,audio $USER
+passwd $USER
+#editing the sudeors file to give members of wheel group to get sudo access
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+echo "----------------------------------------------------------------------------------------------------------"
+echo "-- Setup Dependencies--"
+echo "----------------------------------------------------------------------------------------------------------"
+pacman -S networkmanager network-manager-applet wireless_tools git reflector base-devel --noconfirm --needed
+pacman -S grub efibootmgr wpa_supplicant mtools dosfstools linux-headers --noconfirm --needed
+echo "----------------------------------------------------------------------------------------------------------"
+echo "---Inittializing the bootloader---"
+echo "----------------------------------------------------------------------------------------------------------"
+echo "initializing grub"
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl start NetworkManager
+systemctl enable NetworkManager
+echo "DO U NEED TO CLONE POST INSTALLTION SCRIPT (y/n): "
+read CONFIRM_post
+echo "----------------------------------------------------------------------------------------------------------"
+echo "---BASE INSTALLATION FINISHED---"
+echo "----------------------------------------------------------------------------------------------------------"
+echo "YOU CAN REBOOT NOW"
 
-# # setting timezone
-# ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-# hwclock --systohc
-# #localization
-# sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-# locale-gen
-# #create locale.conf
-# echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-# echo "----------------------------------------------------------------------------------------------------------"
-# echo "---Setting up system---"
-# echo "----------------------------------------------------------------------------------------------------------"
-# echo "Enter the hostname"
-# read HOSTNAME
-# echo $HOSTNAME >> /etc/hostname
-# cat <<EOF > /etc/hosts
-# 127.0.0.1	localhost
-# ::1			localhost
-# 127.0.1.1	$HOSTNAME.localdomain	$HOSTNAME
-# EOF
-# echo "root user password"
-# passwd
-# echo "enter the usernmae"
-# read USER
-# useradd -m $USER
-# usermod -aG wheel,storage,power,audio $USER
-# passwd $USER
-# #editing the sudeors file to give members of wheel group to get sudo access
-# sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-# pacman -S grub efibootmgr wpa_supplicant mtools dosfstools linux-headers --noconfirm --needed
-# echo "----------------------------------------------------------------------------------------------------------"
-# echo "---Inittializing the bootloader---"
-# echo "----------------------------------------------------------------------------------------------------------"
-# echo "initializing grub"
-# grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-# grub-mkconfig -o /boot/grub/grub.cfg
-# systemctl start NetworkManager
-# systemctl enable NetworkManager
-# echo "DO U NEED TO CLONE POST INSTALLTION SCRIPT (y/n): "
-# read CONFIRM_post
-# umount -R /mnt
-# echo "----------------------------------------------------------------------------------------------------------"
-# echo "---BASE INSTALLATION FINISHED---"
-# echo "----------------------------------------------------------------------------------------------------------"
-# echo "YOU CAN REBOOT NOW"
+REALEND
 
-# REALEND
+arch-chroot /mnt sh next.sh
 
-# arch-chroot /mnt sh next.sh
-
-# else
-#     echo "System is not booted in uefi mode, Exiting..."
-#     exit 1
+else
+    echo "System is not booted in uefi mode, Exiting..."
+    exit 1
 fi
